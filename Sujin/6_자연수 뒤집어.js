fea07d9bb4fa4d67f5ee4d6abf5741f05011b9f6
Sujin Jeon <mackerel0106@gmail.com>
2023-03-15 20:01:00 +0900

    Add: 5, 6 문제풀이

--- Sujin/6_자연수 뒤집어.js
+++ Sujin/6_자연수 뒤집어.js
@@ -1,2 +1,14 @@
 // 자연수 뒤집어 배열로 만들기
 // 주석으로 여러분의 이름과 코드, 해설을 꼭 적어주세요!
+
+function solution(n) {
+    var answer = [];
+    
+    n = String(n);
+
+    for (let i = n.length - 1; i >= 0; i--) {
+        answer.push(parseInt(n[i]));
+    }
+    
+    return answer;
+}
