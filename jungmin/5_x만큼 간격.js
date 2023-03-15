99dfc9ae23c689452778b620b734bee69a3741a4
jungsmins1 <jungsmins00@gmail.com>
2023-03-15 23:08:17 +0900

    3 - 6 번 문제풀이

--- jungmin/5_x만큼 간격.js
+++ jungmin/5_x만큼 간격.js
@@ -1,2 +1,15 @@
 // x만큼 간격이 있는 n개의 숫자
 // 주석으로 여러분의 이름과 코드, 해설을 꼭 적어주세요!
+
+// 1. 1부터 1씩 증가하며 n까지 반복한다.
+// 2. 1씩 증가하는 n 값에 x를 곱해준다.
+
+function solution(x, n) {
+  let answer = [];
+  
+  for (let i = 1; i <= n; i++) {
+      answer.push(x * i)
+  }
+
+  return answer;
+}
