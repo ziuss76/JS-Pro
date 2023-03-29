2582b846f1aac128d3c9a737f0a53c62a36c1767
hee <mkmc020@gmail.com>
2023-03-30 02:02:33 +0900

    23-03-29

--- Heekyung/3주차/6_수박수박수박수박수박수.js
+++ Heekyung/3주차/6_수박수박수박수박수박수.js
@@ -1,1 +1,13 @@
+//권희경
 
+function solution(n) {
+  var answer = "";
+
+  if (n % 2 === 0) {
+    answer = "수박".repeat(n / 2); //짝수일 경우 "박"으로 끝난다.
+  } else {
+    answer = "수박".repeat((n - 1) / 2) + "수"; // 홀수일 경우 "수"로 끝난다.
+  }
+
+  return answer;
+}
