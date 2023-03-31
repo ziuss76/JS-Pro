09b132c6ecf5e92721876d536f694f2a6b0d4c73
김지우 <ziuss76@gmail.com>
2023-03-31 11:19:42 +0900

    3주차
    3주차

--- Ziu/3주차 8문제/1_핸드폰 번호 가리기.js
+++ Ziu/3주차 8문제/1_핸드폰 번호 가리기.js
@@ -4,4 +4,4 @@ function solution(phone_number) {
 // 뒷 자리 4개 제외한 수 만큼 별 찍기 + -4 인덱스부터 끝까지 slice
 // 사실 폰넘버 원본 데이터를 수정 안하려고 [...phone_number].slice(-4).join('') 했었는데
 // slice 는 원본 수정 X, splice 는 원본 수정 O
-// 웬만하면 slice 쓰쟈
+// 웬만하면 slice 쓰자
