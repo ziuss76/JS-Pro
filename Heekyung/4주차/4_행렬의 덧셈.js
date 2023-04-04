ec0e67a9df7f1def354f73532535088984091d3b
hee <mkmc020@gmail.com>
2023-04-05 00:20:05 +0900

    23-04-04

--- Heekyung/4주차/4_행렬의 덧셈.js
+++ Heekyung/4주차/4_행렬의 덧셈.js
@@ -1,1 +1,12 @@
 //권희경
+
+function solution(arr1, arr2) {
+  var answer = [[]];
+  for (var i = 0; i < arr1.length; i++) {
+    answer[i] = [];
+    for (var j = 0; j < arr1[i].length; j++) {
+      answer[i].push(arr1[i][j] + arr2[i][j]);
+    }
+  }
+  return answer;
+}
