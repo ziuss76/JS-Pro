6ac400d14bfa2d1b59ecdfc80f19266c1347c97a
hee <mkmc020@gmail.com>
2023-04-06 22:00:45 +0900

    23-04-06

--- Heekyung/4주차/8_3진법 뒤집기.js
+++ Heekyung/4주차/8_3진법 뒤집기.js
@@ -1,1 +1,15 @@
 //권희경
+
+function solution(n) {
+  let reverseNum = n.toString(3).split("").reverse().join("");
+  return parseInt(reverseNum, 3);
+}
+
+// 멋진 reduce 풀이
+function solution(n, numbers = []) {
+  while (n) numbers.push(n % 3), (n = parseInt(n / 3));
+  return numbers.reduce(
+    (X, Y, Z, $) => X + Y * Math.pow(3, $.length - Z - 1),
+    0
+  );
+}
